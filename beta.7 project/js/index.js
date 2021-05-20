48f9d870971270b4d2ff63d4ff864e9d3c65d825
egt75 <76556813+egt75@users.noreply.github.com>
2021-05-20 15:02:01 +0300

    pr

---
+++ beta.7 project/js/index.js
@@ -0,0 +1,9 @@
+  $(document).ready(function(){
+  $('.modal').modal();
+  });
+  $(document).ready(function(){
+  $('.collapsible').collapsible();
+  });
+  $(document).ready(function(){
+    $('.tabs').tabs();
+  });
